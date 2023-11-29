3e61cd3247455cbfff6713ff2c5af424d2b194c7
jairrami06 <117552816+jairrami06@users.noreply.github.com>
2023-11-29 00:23:00 -0500

    as

--- proyecto.asm
+++ proyecto.asm
@@ -1,5 +1,4 @@
 name Proyecto
-include 'emu8086.inc'
 org 100h
 
 .model small
@@ -19,121 +18,6 @@ msgs3 db 'Tematica seleccionada: Paises de Asia y de Africa$'
 msgo1 db 10,13,'1.Opcion 1$'
 msgo2 db 10,13,'2.Opcion 2$'
 msgo3 db 10,13,'3.Atras$'
-  
-salto db 10,13,'$'
-msg1.1 db ''
-.code
-.start
-
-
-; Inicio del programa
-inicio: 
-    mov ah, 09h
-    lea dx, msgp
-    int 21h
-    lea dx, msgp0
-    int 21h
-    lea dx, msgp1
-    int 21h
-    lea dx, msgp2
-    int 21h
-    lea dx, msgp3
-    int 21h
-    lea dx, msgp4
-    int 21h
-    lea dx, msgp5
-    int 21h
-    mov ah, 01h
-    int 21h
-    sub al, 30h
-    
-    cmp al, 1
-    je opcion1
-    cmp al, 2
-    je opcion2
-    cmp al, 3
-    je opcion3
-    cmp al, 4
-    je final
-
-opcion1:
-    mov ah, 09h
-    lea dx, msgs1
-    int 21h
-    lea dx, msgo1
-    int 21h
-    lea dx, msgo2
-    int 21h
-    lea dx, msgo3
-    int 21h
-    lea dx, msgp5
-    int 21h
-    mov ah, 01h
-    int 21h
-    sub al, 30h
-    
-    cmp al, 1
-    je printsopa1
-    cmp al, 2 
-    je printsopa2
-    cmp al, 3
-    je inicio 
-    
-opcion2:
-    mov ah, 09h
-    lea dx, msgs2
-    int 21h
-    lea dx, msgo1
-    int 21h
-    lea dx, msgo2
-    int 21h
-    lea dx, msgo3
-    int 21h
-    lea dx, msgp5
-    int 21h
-    mov ah, 01h
-    int 21h
-    sub al, 30h  
-    
-
-opcion3:   
-    print 'Ingrese un numero: '
-    mov ah, 01h
-    int 21h
-    sub al, 30h
-    lea dx, salto
-    mov ah, 09h
-    int 21h
-    sub al, 30h
-    
-    cmp al, 1
-name Proyecto
-include 'emu8086.inc'
-org 100h
-
-.model small
-.data
-msgp db '                     Bienvenido a sopadeletraPOL $'
-msgp0 db 10,13,'Escoge una tematica:$'
-msgp1 db 10,13,'1.Paises de America y de Europa$'
-msgp2 db 10,13,'2.Ciudades del Ecuador$'
-msgp3 db 10,13,'3.Paises de Asia y de Africa$'
-msgp4 db 10,13,'4.Salir$',10,13
-msgp5 db 10,13,'Opcion seleccionada: $'
-
-msgs1 db 'Tematica seleccionada: Paises de America y Europa$'
-msgs2 db 'Tematica seleccionada: Ciudades del Ecuador$'
-msgs3 db 'Tematica seleccionada: Paises de Asia y de Africa$'
-
-msgo1 db 10,13,'1.Opcion 1$'
-msgo2 db 10,13,'2.Opcion 2$'
-msgo3 db 10,13,'3.Atras$'
-
-palabras db 'BRASIL', 0, 'PERU', 0, 'FRANCIA', 0, 'CHILE', 0, 'ALEMANIA', 0
-
-bufferUsuario db 50 ; Tamaño máximo de la cadena (por ejemplo, 50 caracteres)
-               db ?  ; Longitud de la cadena ingresada (se actualizará automáticamente)
-               db 50 dup(0) ; Reservar 50 bytes para la cadena
   
 salto db 10,13,'$'
 msg1.1 db ''     
@@ -215,7 +99,6 @@ opcion2:
     
 
 opcion3:   
-    print 'Ingrese un numero: '
     mov ah, 01h
     int 21h
     sub al, 30h
@@ -274,4 +157,3 @@ clearscreen:
     mov al, 3  ; video mode 3, which is text mode 80x25
     int 10h  
     ret  ; call interrupt 10h    
-
