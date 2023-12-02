32a37bf94547c62e9ecab6614abcafa94929ce80
jairrami06 <117552816+jairrami06@users.noreply.github.com>
2023-12-02 14:01:33 -0500

    Case sensitive

--- proyecto.asm
+++ proyecto.asm
@@ -11,25 +11,31 @@ msgp3 db 10,13,'3.Paises de Asia y de Africa$'
 msgp4 db 10,13,'4.Salir$',10,13
 msgp5 db 10,13,'Opcion seleccionada: $'
 
-msgs1 db 'Tematica seleccionada: Paises de America y Europa$'
-msgs2 db 'Tematica seleccionada: Ciudades del Ecuador$'
-msgs3 db 'Tematica seleccionada: Paises de Asia y de Africa$'
+msgs1 db 'Tematica : Paises de America y Europa$'
+msgs2 db 'Tematica : Ciudades del Ecuador$'
+msgs3 db 'Tematica : Paises de Asia y de Africa$'
 
-msgo1 db 10,13,'1.Opcion 1$'
-msgo2 db 10,13,'2.Opcion 2$'
+msgo1 db 10,13,'1.Facil$'
+msgo2 db 10,13,'2.Dificil$'
 msgo3 db 10,13,'3.Atras$'
 
 msgj1 db 10,13,'Palabra (0 para rendirse): $'
 
 msgf1 db '               Juego Terminado $'
 msgf2 db 10,13,'Felicidades!!! Ha adivinado las 5 palabras$'
-msgf3 db 10,13,'Se ha rendido$'
+msgf3 db 10,13,'Muchas gracias por jugar$'
 
 ; Lista de palabras
-words db 'brasil$francia$peru$chile$alemania$', 0
+words1_1 db 'brasil$francia$peru$chile$alemania$', 0
+words1_2 db 'brasil$francia$peru$chile$alemania$', 0
+words2_1 db 'brasil$francia$peru$chile$alemania$', 0
+words2_2 db 'brasil$francia$peru$chile$alemania$', 0
+words3_1 db 'brasil$francia$peru$chile$alemania$', 0
+words3_2 db 'brasil$francia$peru$chile$alemania$', 0
+
 wordEnd db 0
-punteroasopa dw ?
-punteroapalabras dw ?
+pointersopa dw ?
+pointerwords dw ?
 contador db ?
 
 ; Buffer para la palabra del usuario, ajustado para el servicio 0Ah
@@ -94,9 +100,9 @@ opcion1:
     sub al, 30h
     
     cmp al, 1
-    je printsopa1
+    je printsopa1.1
     cmp al, 2 
-    je printsopa2
+    je printsopa1.2
     cmp al, 3
     je inicio 
     
@@ -114,50 +120,123 @@ opcion2:
     int 21h
     mov ah, 01h
     int 21h
-    sub al, 30h  
+    sub al, 30h
+    
+    cmp al, 1
+    je printsopa2.1
+    cmp al, 2 
+    je printsopa2.2
+    cmp al, 3
+    je inicio   
     
 
 opcion3:   
-    mov ah, 01h
-    int 21h
-    sub al, 30h
-    lea dx, salto
     mov ah, 09h
+    lea dx, msgs2
+    int 21h
+    lea dx, msgo1
+    int 21h
+    lea dx, msgo2
+    int 21h
+    lea dx, msgo3
+    int 21h
+    lea dx, msgp5
+    int 21h
+    mov ah, 01h
     int 21h
     sub al, 30h
     
     cmp al, 1
-    je printsopa1
-    cmp al, 2
-    je printsopa2
+    je printsopa3.1
+    cmp al, 2 
+    je printsopa3.2
+    cmp al, 3
+    je inicio  
     
-printsopa1:          
-sopa1 DB 10,13,'BRASILXXXXXXX',0Dh,0Ah,'XXXXXXPERUXXX',0Dh,0Ah
-     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
-     DB 'FRANCIAXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
-     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'CHILEXXXXXXXX',0Dh,0Ah
-     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
-     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXALEMANIAX',0Dh,0Ah
-     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'$'
-    mov punteroasopa, offset sopa1
-    mov punteroapalabras, offset words
+printsopa1.1:       
+    sopa1_1 DB 10,13,'I F H B R A S I L C F S M',0Dh,0Ah, 'V I A E K B P C I U M N B',0Dh,0Ah
+    DB 'L P O I Q S E H A Q W T E',0Dh,0Ah,'A F R T Q W R A S D J K L',0Dh,0Ah
+    DB 'A C R N T Y U Z X C V B U',0Dh,0Ah,'X Q Z A I A M O N X Z B N',0Dh,0Ah
+    DB 'X C B V N H I L A P L Q S',0Dh,0Ah,'Z P O L S C Q J A A N J H',0Dh,0Ah
+    DB 'Q O L S Z M I N B T R E W',0Dh,0Ah,'A L E M A N I A I E K S N',0Dh,0Ah
+    DB 'X C A S E W O J K N M D A',0Dh,0Ah,'E I Q K S E L I H C S J U',0Dh,0Ah
+    DB 'Q U W K S M M N X B H J O',0Dh,0Ah,'$'
+    mov pointersopa, offset sopa1_1
+    mov pointerwords, offset words1_1
     call imprimir
     jmp principal
     
-printsopa2:   
-sopa2 DB 10,13,'ECUADORXXXXXX',0Dh,0Ah,'XXXXXXPERUXXX',0Dh,0Ah
+printsopa1.2:  
+sopa1_2 DB 10,13,'ECUADORXXXXXX',0Dh,0Ah,'XXXXXXPERUXXX',0Dh,0Ah
      DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
      DB 'USAXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
      DB 'XXXXXXXXXXXXX',0Dh,0Ah,'CHILEXXXXXXXX',0Dh,0Ah
      DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
      DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXALEMANIAX',0Dh,0Ah
      DB 'XXXXXXXXXXXXX',0Dh,0Ah,'$'
-    mov dx, offset sopa2 ; Puntero al inicio de la sopa de letras
-    jmp imprimir
+    mov pointersopa, offset sopa1_2
+    mov pointerwords, offset words1_2
+    call imprimir
+    jmp principal
+    
+printsopa2.1:
+    mov ah, 09h
+    lea dx, msgs2
+    int 21h    
+sopa2_1 DB 10,13,'ECUADORXXXXXX',0Dh,0Ah,'XXXXXXPERUXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'USAXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'CHILEXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXALEMANIAX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'$'
+    mov pointersopa, offset sopa2_1
+    mov pointerwords, offset words2_2
+    call imprimir
+    jmp principal
+    
+printsopa2.2:   
+sopa2_2 DB 10,13,'ECUADORXXXXXX',0Dh,0Ah,'XXXXXXPERUXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'USAXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'CHILEXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXALEMANIAX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'$'
+    mov pointersopa, offset sopa2_2
+    mov pointerwords, offset words2_2
+    call imprimir
+    jmp principal  
+    
+printsopa3.1:   
+sopa3_1 DB 10,13,'ECUADORXXXXXX',0Dh,0Ah,'XXXXXXPERUXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'USAXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'CHILEXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXALEMANIAX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'$'
+    mov pointersopa, offset sopa3_1
+    mov pointerwords, offset words3_1
+    call imprimir
+    jmp principal
+  
+printsopa3.2:   
+sopa3_2 DB 10,13,'ECUADORXXXXXX',0Dh,0Ah,'XXXXXXPERUXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'USAXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'CHILEXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXXXXXXXXXX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'XXXXALEMANIAX',0Dh,0Ah
+     DB 'XXXXXXXXXXXXX',0Dh,0Ah,'$'
+    mov pointersopa, offset sopa3_2
+    mov pointerwords, offset words3_2
+    call imprimir
+    jmp principal
   
 imprimir:    
-    mov dx, punteroasopa ; Puntero al inicio de la sopa de letras
-    mov si, punteroapalabras
+    mov dx, pointersopa ; Puntero al inicio de la sopa de letras
+    mov si, pointerwords
     call clearscreen 
     mov ah, 09h          ; imprimir cadena
     int 21h              ; Interrupcion del DOS para imprimir
@@ -171,20 +250,41 @@ principal:
     int 21h
     lea dx, salto
     int 21h
-    mov dx, offset userWord
+    lea dx, userWord
     mov ah, 0Ah
     int 21h
-    jmp verificar
+    jmp convertir_a_minusculas
+
+convertir_a_minusculas:
+    lea bx, [userWord + 2]      ; Comienza en el tercer byte de userWord
+    mov cl, [userWord + 1]      ; Carga el número de caracteres ingresados en CL
+    or cl, cl                   ; Verifica si CL es 0 (ningún carácter ingresado)
+    jz verificar                ; Si es 0, salta directamente a verificar
+
+    convertir_loop:
+        mov al, [bx]            ; Carga el carácter actual en AL
+        cmp al, 'A'             ; Comprueba si es una letra mayúscula
+        jb siguiente_caracter
+        cmp al, 'Z'
+        ja siguiente_caracter
+        add al, 32              ; Convierte la letra a minúscula
+        mov [bx], al            ; Guarda la letra convertida
+
+    siguiente_caracter:
+        inc bx                  ; Avanza al siguiente carácter
+        dec cl                  ; Decrementa el contador de caracteres
+        jnz convertir_loop      ; Continúa si aún hay caracteres por convertir
+
+    jmp verificar               ; Salta a verificar después de convertir
 
 verificar:    
-    mov al, [userWord + 1] ; Carga el número de caracteres ingresados
-    cmp al, 1              ; Compara si solo se ingresó 1 carácter
-    jne NextWord           ; Salta a la etiqueta 'not_zero' si hay más de 1 carácter
+    mov al, [userWord + 1]      ; Carga el número de caracteres ingresados
+    cmp al, 1                   ; Compara si solo se ingresó 1 carácter
+    jne NextWord                ; Salta a la etiqueta 'NextWord' si hay más de 1 carácter
 
-    mov al, [userWord + 2] ; Carga el primer (y único) carácter ingresado
-    cmp al, '0'            ; Compara con el carácter '0'
-    je final             ; Salta a 'is_zero' si el carácter es '0' 
-
+    mov al, [userWord + 2]      ; Carga el primer (y único) carácter ingresado
+    cmp al, '0'                 ; Compara con el carácter '0'
+    je final                    ; Salta a final si es '0'
 NextWord:
     call CompareWords
     jc PrintMessageFound  ; Si se encontró la palabra, salta a PrintWordFound
@@ -212,7 +312,9 @@ CompareWords:
     push cx            ; Guarda el valor original de CX
 
     mov di, offset userWord + 2 ; DI apunta al inicio de userWord
-    mov cx, 20                  ; Longitud máxima de userWord
+    mov cx, 20                  ; Longitud máxima de userWord 
+    mov bx, si                   ; BX guarda la posición inicial de la palabra en la lista
+
 
 CompareLoop:
     mov al, [si]                ; Carga un carácter de la palabra de la lista
@@ -243,7 +345,7 @@ NotEqual:
     clc                         ; Limpia el flag de carry
     ret                         ; Retorna sin encontrar coincidencia
 
-Equal:
+Equal: 
     pop cx                      ; Restaura CX
     pop di                      ; Restaura DI
     pop si                      ; Restaura SI
